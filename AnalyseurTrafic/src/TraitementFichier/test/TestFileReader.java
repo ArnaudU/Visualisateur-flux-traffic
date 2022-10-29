d9e85f66ad9b2f782f205e0effee56b9e6d5d761
Arnaud Uthayakumar <uthayakumar.arnaud@gmail.com>
2022-10-29 22:43:20 +0200

    Changement du nom de la classe

--- AnalyseurTrafic/src/TraitementFichier/test/TestFileReader.java
+++ AnalyseurTrafic/src/TraitementFichier/test/TestFileReader.java
@@ -2,7 +2,7 @@ package TraitementFichier.test;
 
 import TraitementFichier.FileReader;
 
-public class FileReaderTest {
+public class TestFileReader {
     public static void main(String[] args){
         FileReader fr = new FileReader("data/trame.txt");
         System.out.println(fr);
