3470e55c8e898e349e21cb920a6a2baf34f6efa8
ArnaudU <97902519+ArnaudU@users.noreply.github.com>
2022-12-04 10:55:26 +0100

    Delete AnalyseurTrafic directory

--- AnalyseurTrafic/src/Main/Main.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import viewer.FacadeGraphique;
-
-public class Main {
-    public static void main(String[] args) {
-        new FacadeGraphique();
-    }
-}
--- AnalyseurTrafic/src/viewer/FacadeGraphique.java
+++
@@ -1,9 +0,0 @@
-package viewer;
-
-public class FacadeGraphique {
-    public FacadeGraphique(){
-        
-        FenetreEnCours fenetreEnCours = new FenetreEnCours();
-        fenetreEnCours.start();
-    }
-}
